280a721419390bf75f3b987a1c0dfce90ba54513
Aditya Wanwade <Adiwanwade@gmail.com>
2024-05-07 14:34:41 +0530

    still Trying

--- frontend/src/home/pages/Home.jsx
+++ frontend/src/home/pages/Home.jsx
@@ -3,7 +3,7 @@ const Home = () => {
     return (
         <>
         <div className="w-full flex place-items-center justify-center p-6 text-lg">Home!</div>
-        <ContentBox title={"About Me"} content={`I am a full stack developer with experience in both front and back end development.I am trying.`}/>    
+        <ContentBox title={"About Me"} content={`I am a full stack developer with experience in both front and back end development.`}/>    
         
         </>
     );
