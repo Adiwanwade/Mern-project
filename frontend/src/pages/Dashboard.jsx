df0a978ab195164c48643d4295b7c0dc592f6a1d
Aditya Wanwade <Adiwanwade@gmail.com>
2024-05-09 00:47:41 +0530

    add dashboard overview to the admin dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import DashSidebar from '../components/DashSidebar';
 import DashProfile from '../components/DashProfile';
-
+import DashboardComp from '../components/DashboardComp';
 
 export default function Dashboard() {
   const location = useLocation();
@@ -22,6 +22,14 @@ export default function Dashboard() {
     </div>
     {/* profile... */}
     {tab === 'profile' && <DashProfile />}
+     {/* posts... */}
+     {tab === 'posts' && <DashPosts />}
+      {/* users */}
+      {tab === 'users' && <DashUsers />}
+      {/* comments  */}
+      {tab === 'comments' && <DashComments />}
+      {/* dashboard comp */}
+      {tab === 'dash' && <DashboardComp />}
   </div>
 );
 }
