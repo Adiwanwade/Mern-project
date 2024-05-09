3c40892e27501e1d4f3857148ec59aacc21de8ad
Aditya Wanwade <Adiwanwade@gmail.com>
2024-05-09 08:50:43 +0530

    add comment section to the post page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,13 +11,13 @@ import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute';
 import CreatePost from './pages/CreatePost';
 import UpdatePost from './pages/UpdatePost';
 import PostPage from './pages/PostPage';
-// import ScrollToTop from './components/ScrollToTop';
+import ScrollToTop from './components/ScrollToTop';
 import Search from './pages/Search';
 
 export default function App() {
   return (
     <BrowserRouter>
-      {/* <ScrollToTop /> */}
+      <ScrollToTop />
       <Header />
       <Routes>
         <Route path='/' element={<Home />} />
